4d000da4d3deab63cef4ef8827159a8151863773
Thomas Meulendijks <t.meulendijks@outlook.com>
2019-01-08 00:05:02 +0100

    V2.0.1 styling and 404 page

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,6 +1,7 @@
 <template>
 	<router-link :to="item.link" class="item">
-		<h3> {{ item.name}} </h3>
+		<div class="img"></div>
+		<div class="header"> {{ item.name}} </div>
 	</router-link>
 </template>
 <script>
@@ -13,7 +14,27 @@ export default {
   height: 100px;
   width: 100px;
   float: left;
-  background: lightgray;
-  margin: 15px;
+	margin: 15px;
+  text-decoration: none;
+	padding: 10px;
+}
+.item:hover{
+	background: #e0e0e0;
+}
+.img{
+	width:auto;
+	height:75%;
+  background-image: url("../assets/T.png");
+	background-size: contain;
+	background-repeat: no-repeat;
+	background-position: center;
+}
+.header{
+	overflow: auto;
+	height:15%;
+	width:auto;
+	text-align:center;
+	font-size: 100%;
+	padding-top: 10%;
 }
 </style>
---
+++ src/views/404.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="404">
+		<router-link to="/">
+			<h1 id="gohome">you're lost.</h1>
+			<h1 id="gohome">lets go home.</h1>
+			</router-link>
+	</div>
+</template>
+<style lang="scss">
+#gohome{
+text-align:center;
+}
+</style>
