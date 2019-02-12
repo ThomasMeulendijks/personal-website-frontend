50b185261ea2efb68c872fb5efe466e6dc1083cc
Thomas Meulendijks <t.meulendijks@outlook.com>
2019-02-12 10:00:55 +0100

    changed api port

--- src/services/pageService.js
+++ src/services/pageService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
 	async get(name) {
-		const response = await axios.get(`http://localhost:8090/pages/${name}`);
+		const response = await axios.get(`http://localhost:8070/pages/${name}`);
 		return response.data;
 	}
 };
